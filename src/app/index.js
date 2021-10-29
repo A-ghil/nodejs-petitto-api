ece381efa25370ebe2d078949599aaea1723c2ce
Ingrydt Alcântara <ingrydtalcantara@outlook.com>
2021-10-28 21:14:51 -0300

    preparando backend para criar landpage

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const app = express();
 const router = require('../routers')
 
@@ -10,5 +11,6 @@ app.use(cors())
 app.use(express.json());
 
 app.use('/api',router)
+app.use('/', express.static(path.join(__dirname, '..','..','public')))
 
 module.exports = app
